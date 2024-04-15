a6d6539ff922c1174c56e9a3e56208a66459e8e8
Your Name <Your Email>
2024-04-15 12:29:26 -0400

    finished work

---
+++ problems/09-count-scores.js
@@ -0,0 +1,50 @@
+/***********************************************************************
+Write a function `countScores(people)` that takes in an array of score
+objects (people) as its input. A score object has two key-value pairs:
+the scorer (string) and a point value (number). `countScores(people)` should
+return an object that has key-value pairs listing each person who scored as a key
+and the sum of the total points for the game as their value.
+
+
+Example 1:
+let ppl = [{name: "Anthony", score: 10},
+            {name: "Fred", score : 10},
+            {name: "Anthony", score: -8},
+            {name: "Winnie", score: 12}];
+
+console.log(countScores(ppl)); //=> { Anthony: 2, Fred: 10, Winnie: 12 }
+
+Example 2:
+let peeps = [
+  {name: "Anthony", score: 2},
+  {name: "Winnie", score: 2},
+  {name: "Fred", score: 2},
+  {name: "Winnie", score: 2},
+  {name: "Fred", score: 2},
+  {name: "Anthony", score: 2},
+  {name: "Winnie", score: 2}
+];
+console.log(countScores(peeps)); //=> { Anthony: 4, Fred: 4, Winnie: 6 }
+***********************************************************************/
+
+let countScores = function(people) {
+  // Your code here
+
+  const scoreTotal = {};
+
+  for(let i=0; i<people.length; i++){
+
+    let personObj = people[i];
+    let person = personObj.name;
+    let points = personObj.score;
+
+    if(!scoreTotal[person]){
+      scoreTotal[person]=0;
+    }
+    scoreTotal[person]+=points;
+  }
+  return scoreTotal;
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = countScores;
